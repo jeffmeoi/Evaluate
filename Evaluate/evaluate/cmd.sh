edb478eeb87855b9f5e5c048a1ac70bb510e3a9f
Jeff <jeffmeoi@qq.com>
2019-01-22 02:36:28 +0800

    删除无用项

--- Evaluate/evaluate/cmd.sh
+++
@@ -1,6 +0,0 @@
-#make program
-gcc evaluate.c ../utils/filepath.c ../models/submission.c ../models/group.c ../models/process.c ../utils/str.c -o evaluate
-
-#running program
-./evaluate 1 256 2 /home/jeffxie/Documents/test/helloworld/ /home/jeffxie/Documents/test/test_program test_program
-
